9c6da195fa688c1f56ec84c7b0e9469515ed51e0
Mahabub Islam <miprio101@gmail.com>
2023-12-21 03:02:38 +0600

    controller: api endpoint to get the translated data from the API

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-// index.js
-const express = require('express');
-const app = express();
-const port = 3000;
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+// init the configuration environment
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const app = express();
+app.use(bodyParser.json())
+import openaiRoutes from './routes/openaiRoutes.js';
+export const completionUrl = process.env.COMPLETION_URL || 'https://api.openai.com/v1/engines/davinci/completions';
+export const model = process.env.MODEL || 'gpt-3.5-turbo';
+export const secret = process.env.OPEN_API_KEY;
+
+// Use routes
+app.use('/api', openaiRoutes);
+
+// Set up the Express app to listen on a specific port
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
