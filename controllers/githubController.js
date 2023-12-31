06de3a11f391128b1e0f2ca70575d6829baa911e
Mahabub Islam <miprio101@gmail.com>
2023-12-31 19:53:38 +0600

    feat: added the collection for the apis

--- controllers/githubController.js
+++ controllers/githubController.js
@@ -14,7 +14,7 @@ export const signIn = async (req, res) => {
   let response = await axios.get(`${github_base_url}/user`, {
         headers: headersLoad,
   });
-  
+
   res.status(200).json({data: response.data });
 }
 
@@ -36,24 +36,6 @@ export const getListOfRepos = async (req, res) => {
     res.status(200).json({ data: "Hello World" });
 }
 
-
-// get the list of the repo prs
-// param:
-//   - access_token
-//   - username
-//   - repo
-// return:
-//   - list of prs
-
-export const getListOfPRs = async (req, res) => {
-    // hit the github api to get the response with access token
-    if (!req.body) {
-        return res.status(400).send({
-        message: "Request body can not be empty."
-        });
-    }
-    res.status(200).json({ data: "Hello World" });
-}
 
 // get the list of the repo issues
 // param:
@@ -82,6 +64,61 @@ export const getListOfIssues = async (req, res) => {
     res.status(200).json({ data: response.data });
 }
 
+//===================================================================================================
+// PR Related APIs
+//===================================================================================================
+// get the list of the repo prs
+// param:
+//   - access_token
+//   - username
+//   - repo
+// return:
+//   - list of prs
+// URL Format: https://api.github.com/repos/OWNER/REPO/pulls
+
+export const getListOfPRs = async (req, res) => {
+    // hit the github api to get the response with access token
+    if (!req.body) {
+        return res.status(400).send({
+        message: "Request body can not be empty."
+        });
+    }
+    let owner = req.body.data.owner;
+    let repo = req.body.data.repo;
+
+    let response = await axios.get(`${github_base_url}/repos/${owner}/${repo}/pulls`, {
+        headers: headersLoad,
+    })
+    res.status(200).json({ data: response.data });
+}
+
+// get the pr of the repo prs
+// param:
+//   - access_token
+//   - username
+//   - repo
+//   - prnumber
+// return:
+//   - pr
+// URL Format: https://api.github.com/repos/OWNER/REPO/pulls/PULL_NUMBER
+
+export const getPR = async (req, res) => {
+    // hit the github api to get the response with access token
+    if (!req.body) {
+        return res.status(400).send({
+        message: "Request body can not be empty."
+        });
+    }
+    let owner = req.body.data.owner;
+    let repo = req.body.data.repo;
+    let prnumber = req.body.data.prnumber;
+
+    let response = await axios.get(`${github_base_url}/repos/${owner}/${repo}/pulls/${prnumber}`, {
+        headers: headersLoad,
+    })
+    res.status(200).json({ data: response.data });
+}
+
 // get the comments of the repo prs
 // param:
 //   - access_token
@@ -90,15 +127,24 @@ export const getListOfIssues = async (req, res) => {
 //   - pr
 // return:
 //   - list of comments
+// URL Format: https://api.github.com/repos/OWNER/REPO/pulls/PULL_NUMBER/comments
 
 export const getListOfPRComments = async (req, res) => {
     // hit the github api to get the response with access token
     if (!req.body) {
         return res.status(400).send({
-        message: "Request body can not be empty."
+            message: "Request body can not be empty."
         });
     }
-    res.status(200).json({ data: "Hello World" });
+
+    let owner = req.body.data.owner;
+    let repo = req.body.data.repo;
+    let prnumber = req.body.data.prnumber;
+
+    let response = await axios.get(`${github_base_url}/repos/${owner}/${repo}/pulls/${prnumber}/comments`, {
+        headers: headersLoad,
+    })
+    res.status(200).json({ data: response.data });
 }
 
 // write the comments of the repo prs
@@ -109,7 +155,8 @@ export const getListOfPRComments = async (req, res) => {
 //   - pr
 //   - comment
 // return:
-//   - list of comments
+//   - PR
+// URL: https://api.github.com/repos/OWNER/REPO/pulls/PULL_NUMBER/comments
 
 export const writePRComment = async (req, res) => {
     // hit the github api to get the response with access token
@@ -118,6 +165,58 @@ export const writePRComment = async (req, res) => {
         message: "Request body can not be empty."
         });
     }
-    res.status(200).json({ data: "Hello World" });
+    let owner = req.body.data.owner;
+    let repo = req.body.data.repo;
+    let prnumber = req.body.data.prnumber;
+    let comment = req.body.data.comment;
+
+    let requestData = {
+        "body": comment
+    };
+
+    let response = await axios.post(`${github_base_url}/repos/${owner}/${repo}/pulls/${prnumber}/comments`, requestData, {
+        headers: headersLoad,
+    })
+
+    res.status(200).json({ data: response.data });
 }
 
+
+// Create a review reply
+// param:
+//   - access_token
+//   - username
+//   - repo
+//   - pr
+//   - comment
+// return:
+//   - PR
+// URL: https://api.github.com/repos/OWNER/REPO/pulls/PULL_NUMBER/comments/COMMENT_ID/replies
+
+export const createPRReviewReply = async (req, res) => {
+    // hit the github api to get the response with access token
+    if (!req.body) {
+        return res.status(400).send({
+        message: "Request body can not be empty."
+        });
+    }
+    let owner = req.body.data.owner;
+    let repo = req.body.data.repo;
+    let prnumber = req.body.data.prnumber;
+    let comment_id = req.body.data.comment_id;
+    let comment = req.body.data.comment;
+
+    let requestData = {
+        "body": comment
+    };
+
+    let response = await axios.post(`${github_base_url}/repos/${owner}/${repo}/pulls/${prnumber}/comments/${comment_id}/replies`, requestData, {
+        headers: headersLoad,
+    })
+
+    res.status(200).json({ data: response.data });
+}
+
+//===================================================================================================
+// End of PR Related APIs
+//===================================================================================================
