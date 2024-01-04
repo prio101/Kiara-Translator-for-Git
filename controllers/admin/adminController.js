ebea349d2106fea24748a700bdc4d9a574e3c001
Mahabub Islam <miprio101@gmail.com>
2024-01-04 14:27:39 +0600

    remove: files

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -1,4 +1,5 @@
 import Setting from '../../models/setting.js';
+import { seedSettings } from '../../seeds/settings.js';
 
 
 export const getDashboard = (req, res) => {
@@ -17,20 +18,34 @@ export const getSetting = (req, res) => {
 
 export const createSetting = async (req, res) => {
     res.render('admin/settings/new', { title: 'Settings' });
-    // try {   
-    //     if(res.status == 201) {
-    //         res.redirect('/web/admin/settings');
-    //     }
-    //     else {
-    //         throw Exception('Error creating setting');
-    //     }
-    // } catch (error) {
-    //     console.log(error);
-    //     res.render('admin/settings/new', { title: 'Settings' });
-    // }
 }
 
 
 export const getAction = (req, res) => {
     res.render('admin/actions/new', { title: 'Actions' });
+}
+
+
+// Responsible for the Syncing of the database
+// With Model Attributes.
+// Should be ran after any Migration Changes happened into the model schema.
+export const getSync = async (req, res) => {
+    try {
+        Setting.sync({ alter: true });
+        let settings = await Setting.findAll();
+        res.status(200).json({data: "Updated the schema successfully", settings: settings});
+    } catch (error) {
+        Setting.sync({ alter: true });
+        res.status(500).json({ error: error.message });
+    }
+}
+
+
+export const postSeed = async (req, res) => {
+    try {
+        let result = await seedSettings();
+        res.status(200).json({data: "Seeded the database successfully", result: result});
+    }catch{
+        res.status(500).json({ error: error.message });
+    }
 }
